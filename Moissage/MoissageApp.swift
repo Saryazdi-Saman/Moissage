ea882c078f3b2138fc29277e3692a61f968edd8c
Saman Saryazdi <Saman.Saryazdi@gmail.com>
2022-10-10 18:55:38 -0400

    Initial Commit

---
+++ Moissage/MoissageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoissageApp.swift
+//  Moissage
+//
+//  Created by Saman Saryazdi on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct MoissageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
